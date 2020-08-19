958f66c3ecaf36fa0f5abefdf9c9827fc4c411d0
Mack Liu <bazhe1106@gmail.com>
2020-08-19 16:27:48 +0800

    [Add] 建立並加入新專案

---
+++ kkday-hw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kkday-hw
+//
+//  Created by Mack Liu on 2020/8/19.
+//  Copyright © 2020 eBMW Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
